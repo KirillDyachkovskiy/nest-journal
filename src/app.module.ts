449b661ae8dab674c4372b7fee18483e6ce59953
Kirill <kirilldyachkovskiy@gmail.com>
2022-06-03 17:18:29 +0300

    clean project and add user resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from './users/users.module';
+
+import { UserEntity } from './users/entities/user.entity';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'rootroot',
+      database: 'journal',
+      entities: [UserEntity],
+      synchronize: true,
+    }),
+    UsersModule,
+  ],
 })
 export class AppModule {}
